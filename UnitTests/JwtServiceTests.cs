542869d1efcf8c1304fad8576dca14d8950d4203
Glitched Polygons <glitchedpolygons@protonmail.com>
2018-09-04 15:58:16 +0200

    Added first unit test (ctor).

--- UnitTests/JwtServiceTests.cs
+++ UnitTests/JwtServiceTests.cs
@@ -1,10 +1,18 @@
 using System;
+using System.Security;
 using Xunit;
 
 namespace GlitchedPolygons.Services.JwtService.UnitTests
 {
     public class JwtServiceTests
     {
+        [Fact]
+        public void Ctor_NullOrEmptyKey_ThrowsArgumentException()
+        {
+            Assert.Throws<ArgumentException>(() => new JwtService(null));
+            Assert.Throws<ArgumentException>(() => new JwtService(new SecureString()));
+        }
+
 
     }
 }
